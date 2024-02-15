6d33efa159cefacb5d3437c78609ead56ad52702
FabianFernandez19 <145621184+FabianFernandez19@users.noreply.github.com>
2024-02-15 09:11:15 -0500

    cambie unas cosas del logro controller
    modifique algunas cosas del controlador para que funcione a la hora de enviar la respuesta y guardarla

--- app/Http/Controllers/API/logrosApiController.php
+++ app/Http/Controllers/API/logrosApiController.php
@@ -65,33 +65,41 @@ class LogrosApiController extends Controller
         return response()->json(null, 204);
     }
     
+
     public function asignarLogro(Request $request)
     {
         $request->validate([
-            'tipoLogro' => 'required|string',
             'tiempoSemanal' => 'required|date_format:H:i:s',
-            'dias' => 'required|integer'
+            'dias' => 'required|integer',
+            'usuario_id' => 'required|integer'
         ]);
         
-        $tiempoSemanal = $request->tiempo_semanal;
-        $diasInteraccion = $request->dias_interaccion;
+        $tiempoSemanal = $request->tiempoSemanal;
+        $diasInteraccion = $request->dias;
+        // Corregir el nombre del campo dias_interaccion a dias
     
-        $logros = Logros::where('tiempoSemanal', $tiempoSemanal)
+        $logro = Logros::where('tiempoSemanal', $tiempoSemanal)
             ->where('dias', $diasInteraccion)
             ->first();
     
-        if ($logros) {
+        if ($logro) {
             $new_logro = new User_has_logros();
             $new_logro->user_id = $request->usuario_id;
-            $new_logro->logros_id = $logros->id; // Cambio aquí
+            $new_logro->logros_id = $logro->id; 
             $new_logro->save();
     
-            return response()->json(['logro' => $logros, 'message' => 'Logro asignado correctamente'], 200); // Cambio aquí
+            return response()->json(['logro' => $logro, 'message' => 'Logro asignado correctamente'], 200);
         } else {
             return response()->json(['message' => 'No se encontró ningún logro correspondiente'], 404);
         }
     }
     
+    
+
+
+
+
+
     public function logrosAsignadosAUsuario($usuario_id)
     {
         $logrosAsignados = User_has_logros::where('user_id', $usuario_id)
